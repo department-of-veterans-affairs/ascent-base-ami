d21e7417dfac6d7589be0a94fb2e7100ddc275df
Cloud User <centos@ip-172-31-31-56.ec2.internal>
2017-10-19 18:43:46 +0000

    fixed path issue in file

--- setup_update_dns.sh
+++ setup_update_dns.sh
@@ -2,9 +2,9 @@
 
 #install bind
 
-sudo cp /tmp/dns/add_server_to_dns.sh /root/
+sudo cp /tmp/add_server_to_dns.sh /root/
 
-sudo cp /tmp/dns/update-dns.service /usr/lib/systemd/system/update-dns.service
+sudo cp /tmp/update-dns.service /usr/lib/systemd/system/update-dns.service
 
 sudo systemctl enable update-dns
 
