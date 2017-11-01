a9f7726ddcc5fd6f273dcda9a545319ab9feee83
Cloud User <centos@ip-172-31-31-56.ec2.internal>
2017-11-01 14:24:23 +0000

    updated to use public IP for DNS server calls

--- dns/add_server_to_dns.sh
+++ dns/add_server_to_dns.sh
@@ -18,9 +18,9 @@ else
   AWSSANS=`aws ec2 describe-instances  --instance-ids $INSTANCE_ID --region $AWS_REGION | jq '.Reservations[] | .Instances[] | [(.Tags|from_entries|.SAN)]' |sed 's/\"//g' | sed 's/\]//g' | sed 's/\[//g'`
 
   MASTER_INSTANCE_ID=`aws --output text --region $AWS_REGION ec2 describe-tags --filters 'Name=tag:Type,Values=dns_master' | awk '{print $3}'`
-  MASTER_SERVER_IP=`aws ec2 describe-instances --instance-id $MASTER_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PrivateIpAddress |sed 's/\"//g'`
+  MASTER_SERVER_IP=`aws ec2 describe-instances --instance-id $MASTER_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PublicIpAddress |sed 's/\"//g'`
   SLAVE_INSTANCE_ID=`aws --output text --region $AWS_REGION ec2 describe-tags --filters 'Name=tag:Type,Values=dns_slave' | awk '{print $3}'`
-  SLAVE_SERVER_IP=`aws ec2 describe-instances --instance-id $SLAVE_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PrivateIpAddress |sed 's/\"//g'`
+  SLAVE_SERVER_IP=`aws ec2 describe-instances --instance-id $SLAVE_INSTANCE_ID --region $AWS_REGION | jq .Reservations[].Instances[].PublicIpAddress |sed 's/\"//g'`
   echo $AWSSAN
   echo $PRIVATEDNSNAME
 
@@ -44,7 +44,7 @@ else
     nsupdate /root/nsupdate.txt
   fi
   #update dhcp address and restart network
-  sudo echo "supersede domain-name-servers "$MASTER_SERVER_IP"; "$SLAVE_SERVER_IP";" >> /etc/dhcp/dhclient.conf
+  sudo echo "supersede domain-name-servers "$MASTER_SERVER_IP", "$SLAVE_SERVER_IP";" >> /etc/dhcp/dhclient.conf
   sudo /etc/init.d/network restart
 fi
 
