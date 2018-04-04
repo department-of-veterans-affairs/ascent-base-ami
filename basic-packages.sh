85bf57ed1fec14b71558f5f61d0263134fb2a6c5
jasonluck <jluck@meetveracity.com>
2018-04-04 10:13:50 -0400

    Move aws binary to /usr/bin so it will be in root PATH

--- basic-packages.sh
+++ basic-packages.sh
@@ -10,7 +10,7 @@ sudo systemctl enable ntpd
 # aws cli
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
 
 # jq command line JSON parser
 sudo wget -O /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
